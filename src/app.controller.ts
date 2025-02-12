8e1ab8a602da75b99968023e10b7f99ec9081df0
Mariia <113040756+bzhemba@users.noreply.github.com>
2025-02-12 17:30:34 +0300

    feat: add static

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import {join} from "path";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getIndex(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'pages', 'index.html'));
+  }
+  @Get('add-recipe.html')
+  getAddPage(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'pages', 'add-recipe.html'));
+  }
+
+  @Get('all-recipes.html')
+  getAllRecipesPage(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'pages', 'all-recipes.html'));
+  }
+
+  @Get('notes.html')
+  getNotesPage(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'pages', 'notes.html'));
+  }
+
+  @Get('recipe-gallery.html')
+  getGalleryPage(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'pages', 'recipe-gallery.html'));
+  }
+
+  @Get('table.html')
+  getTablePage(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'pages', 'table.html'));
+  }
+
+  @Get('index.html')
+  getHomePage(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'pages', 'index.html'));
   }
 }
+
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ConfigService} from "@nestjs/config";
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+      AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const configService = app.get(ConfigService);
 
